761f4e0e11e6cef82e02f036cf4d33e5517e3f02
Warda Ayaz <wayaz107@gmail.com>
2020-07-16 04:43:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,9 +5,10 @@ end
 end
 
 
-
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)# code an argument here
+  new_array = []
+  planeteer_calls.collect{|name| new_array << name.capitalize + "!"}
+  new_array
 end
 
 def long_planeteer_calls# code an argument here
