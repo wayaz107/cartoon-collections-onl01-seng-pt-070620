ad98fe9b4b5f64bb8de9e352434f033a7198e439
Warda Ayaz <wayaz107@gmail.com>
2020-07-16 05:36:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,13 +11,11 @@ def summon_captain_planet(planeteer_calls)# code an argument here
   new_array
 end
 
-def long_planeteer_calls(calls)# code an argument here
-if calls.length >= 4
-   true
-else
-  false
+def long_planeteer_calls(array)# code an argument here
+array.any? do |word|
+  word.length > 4
 end
-end
+end 
 
 def find_the_cheese(array)
 array.find do |cheese|
